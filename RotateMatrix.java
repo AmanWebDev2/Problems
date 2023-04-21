cf6cbcbe5e036398c45468ef9fcd654d1ea754b3
aman <webdevaman2@gmail.com>
2023-04-21 13:22:11 +0530

    added rotated matrix

---
+++ RotateMatrix.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class RotateMatrix {
+
+    static void rotate(int arr[][]) {
+        // step1: transpose
+        int m = arr.length;
+        int n = arr[0].length;
+
+        for(int i=0; i<m; i++) {
+            for(int j=i; j<n; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        // step2: swap 
+        for(int i=0; i<m; i++) {
+            int leftColumnIndex = 0;
+            int rightColumnIndex = n-1;
+            
+            while(leftColumnIndex < rightColumnIndex) {
+                int temp = arr[i][leftColumnIndex];
+                arr[i][leftColumnIndex] = arr[i][rightColumnIndex];
+                arr[i][rightColumnIndex] = temp;
+
+                leftColumnIndex++;
+                rightColumnIndex--;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16},
+        };
+
+        rotate(arr);
+        
+        for(var elem: arr) {
+            System.out.println(Arrays.toString(elem));
+        }
+    }
+}
