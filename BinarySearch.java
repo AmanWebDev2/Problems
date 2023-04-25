ae04dbff6654986e52bea3af410e51448c2d0fbe
aman <webdevaman2@gmail.com>
2023-04-25 09:41:48 +0530

    added orderAgnositic

--- BinarySearch.java
+++ BinarySearch.java
@@ -19,16 +19,44 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    // orderAgnostic - to know wether array is sorted in ascending order or not by comparing last and first value
+    static int orderAgnosticBS(int arr[], int target) {
+        int start = 0;
+        int end = arr.length-1;
+        boolean isAsc = arr[start] < arr[end];
+        while(start <= end) {
+            int mid = start + (end-start)/2;
+
+            if(target == arr[mid]) {
+                return mid;
+            }
+
+            if(isAsc) {
+                if(target > arr[mid]) {
+                    start = mid+1;
+                }else {
+                    end = mid -1;
+                }
+            }else {
+                if(target > arr[mid]) {
+                    end = mid-1;
+                }else {
+                    start = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9,10};
-        int target = 10;
+        int target = 102;
 
         int foundIndex = getIndex(arr, target);
-        if(foundIndex == -1) {
-            System.out.println("no element found");
-        }else {
-            System.out.println("Element found at: "+foundIndex);
-        }
+        System.out.println(foundIndex);
+        
+        System.out.println(orderAgnosticBS(arr, target));
 
     }
 }
